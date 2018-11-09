839a47ad1e1a4b7bd206a43ece452b3c939f69f1
nguardadov <nguardadov@gmail.com>
2018-11-08 22:36:19 -0600

    controladores

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php include('layouts/link.php');?>
-    <title>Practica 10</title>
-</head>
-
 <body>
-
     <header class="mt-1">
         <h1 class="text-center">
             Autenticación en

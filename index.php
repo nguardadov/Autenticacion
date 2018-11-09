14baaf02e9e4ca278e40fc7e6931b425f923380a
nguardadov <nguardadov@gmail.com>
2018-11-09 00:43:55 -0600

    controlador

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
     <?php include('layouts/link.php');?>
     <div class="container mt-5">
         <p>
-            <a href="pages/creacion_proyecto.php" class="btn btn-primary">Creaci&oacute;n del proyecto</a>
+            <a href="pages/creacion_proyecto.php" class="btn btn-primary">Creaci&oacute;n del Proyecto</a>
         </p>
         <p>
-            <a href="pages/creacion_modelos.php" class="btn btn-primary">Creaci&oacute;n de modelos</a>
+            <a href="pages/creacion_modelos.php" class="btn btn-primary">Creaci&oacute;n de Modelos</a>
         </p>
         <p>
-            <a href="pages/creacion_controlador.php" class="btn btn-primary">Creaci&oacute;n de controladores</a>
+            <a href="pages/creacion_controlador.php" class="btn btn-primary">Creaci&oacute;n de Controladores</a>
         </p>
         <p>
-            <a href="pages/creacion_middleware.php" class="btn btn-primary">Creaci&oacute;n de controladores</a>
+            <a href="pages/creacion_middleware.php" class="btn btn-primary">Creaci&oacute;n de Middleware</a>
         </p>
     </div> 
     <?php include('layouts/scripts.php');?>

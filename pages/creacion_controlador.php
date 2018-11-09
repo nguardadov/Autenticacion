57b468fc5682a1a6c2569821deb319f0dba0631b
nguardadov <nguardadov@gmail.com>
2018-11-08 22:51:07 -0600

    layouts

--- pages/creacion_controlador.php
+++ pages/creacion_controlador.php
@@ -1,4 +1,6 @@
 
     <?php include('../layouts/link.php');?>
-    
+    <div class="container">
+        <h2>Creando el controlador</h2>
+    </div>
     <?php include('../layouts/scripts.php');?>  

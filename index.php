99fbdcd72b7f5078c4e598ed872c466b268b64bc
nguardadov <nguardadov@gmail.com>
2018-11-09 01:30:31 -0600

    app.js

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <?php include('layouts/link.php');?>
+<?php include('layouts/link.php');?>
     <div class="container mt-5">
         <p>
             <a href="pages/creacion_proyecto.php" class="btn btn-primary">Creaci&oacute;n del Proyecto</a>
@@ -19,6 +17,9 @@
         </p>
         <p>
             <a href="pages/creacion_vistas.php" class="btn btn-primary">Creaci&oacute;n de Vistas</a>
+        </p>
+        <p>
+            <a href="pages/creacion_app.php" class="btn btn-primary">Configuraci&oacute;n del archivo app.js</a>
         </p>
     </div> 
     <?php include('layouts/scripts.php');?>
